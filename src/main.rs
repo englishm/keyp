f3d8afe1cce0e890286de533e1643e48f17fd914
Mike English <mike.english@gmail.com>
2023-06-12 11:15:42 -0400

    Focus only on request line

--- src/main.rs
+++ src/main.rs
@@ -17,29 +17,59 @@ fn handle_client(stream: TcpStream) -> Result<()> {
 
     // print request as a UTF-8 string
     let s = String::from_utf8(buf.to_vec())?;
-    println!("s: {}", s); // print request
+    println!("{}", s); // print request
 
     // TODO: HTTP Parsing
     // Can we hack together minimal parsing with nothing but std?
     // Or would it be OK to pull in a minmal lib here like httpparse?
     // Let's see how far we can get with something hacky
+    //
+    // TODO: HTTP/2 and HTTP/3 support
+    // This is where we'd probably want to switch over to a library
+
+    // DONE: Ensure we have a complete request before preceding
+    // On second thought, we don't actually need that to fulfill
+    // the requirements as written..
 
     // Note: regex isn't even in std so this could get interesting..
     // Good news is we have slice pattern matching...
-    match buf {
-        [b'G', b'E', b'T', b' ', ..] => {
-            println!("GET request (probably)")
+    let mut lines = s.lines();
+    let request_line;
+    match lines.next() {
+        Some(value) => {
+            request_line = value;
         }
-        [b'P', b'U', b'T', b' ', ..] => {
-            println!("PUT request (probably)")
+        None => {
+            request_line = "";
+            eprintln!("Something went wrong")
+            // TODO: Return 500 error
         }
-        [b'H', b'E', b'A', b'D', b' ', ..] => {
-            println!("HEAD request (probably)")
+    }
+
+    // TODO: handle more registered methods:
+    // https://www.iana.org/assignments/http-methods/http-methods.xhtml
+
+    // HTTP/1.1 Request lines:
+    // https://www.rfc-editor.org/rfc/rfc9112#section-3
+    // request-line   = method SP request-target SP HTTP-version
+    match request_line.split(' ').collect::<Vec<_>>()[..] {
+        ["GET", request_target, http_version] => {
+            println!("GET request");
+            println!("request-target: {}", request_target);
+            println!("HTTP-version: {}", http_version);
         }
-        [b'P', b'O', b'S', b'T', b' ', ..] => {
-            println!("POST request (probably)")
+        ["PUT", request_target, http_version] => {
+            println!("PUT request");
+            println!("request-target: {}", request_target);
+            println!("HTTP-version: {}", http_version);
         }
+        // ["HEAD", request_target, http_version] => {
+        //     println!("HEAD request");
+        //     println!("request-target: {}", request_target);
+        //     println!("HTTP-version: {}", http_version);
+        // }
         _ => {
+            eprintln!("Failed to parse request line: {:?}", request_line);
             todo!("🤷");
         }
     }
