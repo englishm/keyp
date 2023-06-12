02ece392359e7853152b646adff517f0a256ea35
Mike English <mike.english@gmail.com>
2023-06-11 23:24:50 -0400

    Document 'feature' of single-threaded http handling

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,10 @@ fn handle_client(stream: TcpStream) {
 fn main() -> std::io::Result<()> {
     println!("Hello, world!");
     let listener = TcpListener::bind("127.0.0.1:4000")?;
+
+    // Not async or even multi-threaded (yet), but maybe this
+    // is actually a feature for now because we can avoid
+    // data races in our toy database XD
     for stream in listener.incoming() {
         handle_client(stream?);
     }
