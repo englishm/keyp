9dcf98b93259537533c5581bacab499919d4dbbc
Mike English <mike.english@gmail.com>
2023-06-11 23:05:07 -0400

    Listen on tcp/4000 as in stdlib docs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
+use std::net::{TcpListener, TcpStream};
+
+fn handle_client(stream: TcpStream) {
+    println!("handling stream: {:?}", stream);
+}
+
+fn main() -> std::io::Result<()> {
     println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:4000")?;
+    for stream in listener.incoming() {
+        handle_client(stream?);
+    }
+
+    Ok(())
 }
