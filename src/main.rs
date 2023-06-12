e202404b894d999d5139f358f797d9104790ea12
Mike English <mike.english@gmail.com>
2023-06-12 01:45:21 -0400

    Note requirements

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,16 @@ use std::net::{TcpListener, TcpStream};
 
 fn handle_client(stream: TcpStream) {
     println!("handling stream: {:?}", stream);
+    // When your server receives a request on `http://localhost:4000/set?somekey=somevalue`
+    // it should store the passed key and value in memory.
+
+    // When it receives a request on `http://localhost:4000/get?key=somekey`
+    // it should return the value stored at `somekey`.
 }
 
 fn main() -> std::io::Result<()> {
     println!("Hello, world!");
+    // write a program that runs a server that is accessible on `http://localhost:4000/`
     let listener = TcpListener::bind("127.0.0.1:4000")?;
 
     // Not async or even multi-threaded (yet), but maybe this
